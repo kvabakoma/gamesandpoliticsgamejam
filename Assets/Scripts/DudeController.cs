ca31a2acfeaf34103e4f04144cbff49af9e599c8
kvaba <kvabakoma@gmail.com>
2019-09-09 23:07:49 +0200

    added bodyguard

---
+++ Assets/Scripts/DudeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DudeController : MonoBehaviour
+{
+
+    [SerializeField] private float love = 50, health = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am Dude");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
