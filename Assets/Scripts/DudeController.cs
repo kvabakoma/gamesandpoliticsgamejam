d7d78a0ce3db79263dcfb64ecd4ad5402e213689
kvaba <kvabakoma@gmail.com>
2019-09-10 18:11:38 +0200

    i hate tagmanager

--- Assets/Scripts/DudeController.cs
+++ Assets/Scripts/DudeController.cs
@@ -5,16 +5,98 @@ using UnityEngine;
 public class DudeController : MonoBehaviour
 {
 
-    [SerializeField] private float love = 50, health = 100;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float
+          minSpeed = 3f
+        , maxSpeed = 10f
+        , currentSpeed = 3f
+        , minMaxRotationSpeed = 5f
+        , currentRotationSpeed = 1f
+        , nextTimeEvent = 0f
+        , minTimeEvent = 3f
+        , maxTimeEvent = 10f;
+    [SerializeField] private int 
+          love = 50
+        , health = 100
+        , attraction = 100;
+
+    private Rigidbody rb;
+
+
+
+    private enum STATE { WANDERING, FOLLOWING, DANCING, FIGHTING };
+    [SerializeField] private STATE PlayerState = STATE.WANDERING;
+    
+    void Awake()
     {
-        Debug.Log("I am Dude");
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        Debug.Log("I am a Dude");
+    }
+
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        // DIFFERENT BEHAVIOUR BASED ON STATE
+        // if WANDERING - SCAN
+        // IF RELAXING - NOTHING (DELAY WILL EXIT IT)
+        // IF FOLLOWING - NOTHING ( EVENT WILL EXIT IT)
+        // IF DANCING - UPDATE STATS
+        // IF FIGHTING - UPDATE STATS
+        if (PlayerState == STATE.WANDERING)
+        {
+            Scan();
+        }
+        Walk();
+    }
+
+    private void Walk()
+    {
+
+        rb.AddRelativeForce(Vector3.forward * currentSpeed);
+        transform.Rotate(Vector3.up * currentRotationSpeed);
+
+        if (Time.time > nextTimeEvent) ChangeWalkingMode();
+
+    }
+
+    private void ChangeWalkingMode()
+    {
+        nextTimeEvent = Time.time + Random.Range(minTimeEvent, maxTimeEvent);
+        int draw = Random.Range(1, 8);
+        Debug.Log("DRAW:" + draw);
+        if (draw < 5) // walk straight
+        {
+            currentRotationSpeed = 0;
+            currentSpeed = Random.Range(minSpeed, maxSpeed);
+        }
+        else if (draw < 8) // walk with turn
+        {
+            currentRotationSpeed = Random.Range(-minMaxRotationSpeed, minMaxRotationSpeed);
+            currentSpeed = Random.Range(minSpeed, maxSpeed);
+        }
+        else
+        {
+            currentRotationSpeed = 0;
+            currentSpeed = 0;
+        }
+    }
+
+    private void Scan()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 3f, LayerMask.GetMask("humans"));
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            hitColliders[i].SendMessage("AddDamage");
+            i++;
+        }
     }
 }
