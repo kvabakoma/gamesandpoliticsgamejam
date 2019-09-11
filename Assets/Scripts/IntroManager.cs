c941407764b734646038701171b67265629a1074
kvaba <kvabakoma@gmail.com>
2019-09-11 13:25:04 +0200

    final uplaod before presentations

---
+++ Assets/Scripts/IntroManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroManager : MonoBehaviour
+{
+
+    public GameObject introUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("ShowUI", 6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ShowUI()
+    {
+        introUI.SetActive(true);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("HumanSandbox");
+    }
+}
